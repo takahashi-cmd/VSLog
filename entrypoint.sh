faf04531cc673b8df37d982e167f1af5b04a6ef4
hiro_53 <194663299+takahashi-cmd@users.noreply.github.com>
2025-07-10 22:18:58 +0900

    マイグレーション機能追加、Docker初回起動時のスクリプト作成
    マイグレーション機能を追加して、データベースのスキームを後からPython側で変更処理が可能なように設定した。
    Docker起動時に予め必要なテーブル・カラムが設定されるよう、models.pyにクラステーブルを定義した。
    FlaskのDockerfileビルド時にentripoint.shのスクリプトを実行して、マイグレーションの初期化・初期ファイル作成、Flaskアプリの実行を行うように設定した。
    Flaskの環境変数は.envにまとめて記載し、docker compose up時に読み込むように設定した。
    Docker実行後は、migrationsディレクトリがFlaskコンテナに作成され、Flaskコンテナとバインドマウントしているため、ローカルからmigrationsディレクトリの中身が確認可能である。
    作成したテーブル・カラムはversionsの'Initial migration'に保存されるよう設定した。

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# パスの設定
-export PYTHONPATH=/code
+# migrations初期化
+flask db init
 
-# 初回のみ：migrationsディレクトリがなければ初期化
-if [ ! -d "migrations" ]; then
-    flask db init
-fi
+# 初回：マイグレーションファイル作成＋適用
+flask db migrate -m "Initial migration"
 
-# 常時：マイグレーションファイル作成＋適用
-flask db migrate -m "auto migration"
-
-# MySQL が起動するまでリトライしながらマイグレーション適用
-until flask db upgrade; do
-  echo "Waiting for MySQL to be ready..."
-  sleep 2
-done
+# 差分があればマイグレーションをアップデート
+flask db upgrade
 
 # Flaskアプリ起動
 exec flask run --host=0.0.0.0 --debug
