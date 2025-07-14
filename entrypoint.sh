59a56921bcb7acd4fb30522969469c0f91dda03d
hiro_53 <194663299+takahashi-cmd@users.noreply.github.com>
2025-07-14 17:09:12 +0900

    migrations追加・共有、entrypoint.shのマイグレーション自動化を解除、ログイン機能実装
    マイグレーションは開発者が明示的に行う操作であり、毎回実行するものではないため、entrypoint.shの自動化スクリプトを削除した。
    entrypoint.shには、flaskの実行命令のみを記載した。
    migrationの初期化、初期テーブル・カラムの追加は予め行い、開発者で共有しなければ同じ環境下での作業ができないため、migrationsをGitHubに追加・共有する。
    また、ログイン機能を実装し、新規登録したメールアドレス、パスワードをログイン画面に入力すると、ホーム画面（/index）にアクセスできるよう設定した。

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-# migrations初期化
-flask db init
-
-# 初回：マイグレーションファイル作成＋適用
-flask db migrate -m "Initial migration"
-
-# 差分があればマイグレーションをアップデート
-flask db upgrade
-
-# Flaskアプリ起動
+# Flaskアプリ起動（migrateは手動で明示的に実施する）
+# 再利用性や可読性を向上させるため、FlaskのDockerfileには実行コマンドをまとめずに、本ファイルにまとめる。
 exec flask run --host=0.0.0.0 --debug
 
 
