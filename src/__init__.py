0e23d082238264937e7cde0ab1e8d348ebe10cfe
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2025-12-21 15:33:35 +0900

    app.pyを廃止し、create_appファクトリに変更
    ・app.pyに記載していた内容を__init__.py, config.py, extensions.pyに分離
    ・__init__.pyにはcreate_app関数を作成し、ファクトリに変更
    ・config.pyにはFlask, MySQLの設定内容を移植
    ・extensions.pyには、db, migrate, login_managerの拡張機能を移植

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,31 @@
 # アプリファクトリ
+from dotenv import load_dotenv
+from flask import Flask
+
+from .config import Config
+from .extensions import db, migrate, login_manager
+
+def create_app(config_object=Config) -> Flask:
+    # .envファイルの読み込み（カレントディレクトリがルートである前提）
+    load_dotenv()
+    
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+
+    # extensionsを初期化
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    # 未認証のユーザーがリダイレクトされるビュー関数とメッセージを設定
+    login_manager.login_view = 'login_view'
+    login_manager.login_message = 'ログインが必要です。先にログインしてください。'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from .models.users import User
+        return User.query.get(user_id)
+    
+    # from .blueprints import 
+
+    return app
