62882fc4bbb0df0ab51a5cfb74d8b6a481aff504
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2025-12-24 17:50:04 +0900

    title.pyに期間に応じてタイトルを生成する処理を作成

--- src/utils/graphs/types.py
+++ src/utils/graphs/types.py
@@ -1,5 +1,11 @@
 # 共通型定義
 from typing import Literal
 
+# グラフ種類
 GraphType = Literal['bar', 'pie', 'line']
+
+# 縦軸表示形式
 VerticalAxis = Literal['time', 'percent']
+
+# 期間
+Period = Literal['this_week', 'last_week', 'month', 'year', 'all']
